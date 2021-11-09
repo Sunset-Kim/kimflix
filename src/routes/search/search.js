0b127887c0443f06089bc0adf8c59a2439cc02dd
Sunset-Kim <77092632+Sunset-Kim@users.noreply.github.com>
2021-11-10 01:16:27 +0900

    ⚰️  remove input form

--- src/routes/search/search.js
+++ src/routes/search/search.js
@@ -10,22 +10,15 @@ import { debounce } from 'lodash';
 
 const Container = styled.div`
   padding: 10px 10px;
-
 `;
-const SearchBar = styled.form`
-  position: relative;
 
+const SearchBar = styled.div`
+  padding: 0 10px;
+  margin-top: 10px; 
   input {
     width: 100%;
     border-radius: 50px;
     padding: 1rem 1rem;
-  }
-
-  button {
-    position: absolute;
-    right: 1rem;
-    top: 50%;
-    transform: translateY(-50%);
   }
 `;
 
@@ -41,27 +34,29 @@ const Search = (props) => {
   const [query, setQuery] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false)
+
   // ref
   const inputEl = useRef();
   // debouce call
   const debounceCall = debounce((q) => {
     if (query === q) return
     setQuery(q)
-  }, 1500)
+  }, 1000)
 
   const handleChange = () => {
     const newQuery = inputEl.current.value;
     if (!newQuery || newQuery.length === 0) return
     debounceCall(newQuery);
+
   }
 
   const getResult = async (q) => {
     try {
-      setLoading({ loading: true });
+      setLoading(true);
       const { data: { results: currentResult } } = await searchApi.multi(q)
       setCurrentResult(currentResult)
     } catch (error) {
-      setError("is not find result");
+      setError("is not found result");
     } finally {
       setLoading(false)
     }
@@ -83,7 +78,6 @@ const Search = (props) => {
       </Helmet>
       <SearchBar >
         <input type="text" ref={inputEl} placeholder="검색어를 입력하세요" onChange={handleChange} />
-        <button type="submit">검색</button>
       </SearchBar>
       {
         loading ?

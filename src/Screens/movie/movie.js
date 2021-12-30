292f039bd937df66c0654aa8b064553712aa48f3
Sunset-kim <wood2002@naver.com>
2021-12-30 23:59:37 +0900

    🚚 ♻️  라우터 : 라우터폴터이름 변경, router v6 update

--- src/Screens/movie/movie.js
+++ src/Screens/movie/movie.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { moviesApi } from "api"
+import { moviesApi } from "api";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
 import Section from "components/section";
 import Loading from "components/loading";
 import Message from "components/message";
 
-
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const Movie = () => {
+  console.log("movie render");
 
   const [nowPlaying, setNowPlaying] = useState();
   const [upComing, setUpcoming] = useState();
@@ -22,63 +22,69 @@ const Movie = () => {
 
   const getPopular = () => {
     return moviesApi.popular();
-  }
+  };
 
   const getNowPlay = () => {
     return moviesApi.nowPlaying();
-  }
+  };
 
   const getUpComing = () => {
     return moviesApi.upComing();
-  }
+  };
 
   useEffect(async () => {
     try {
-      const [{ data: { results: popular } }, { data: { results: nowPlaying } }, { data: { results: upComing } }] = await Promise.all([getPopular(), getNowPlay(), getUpComing()]);
+      const [
+        {
+          data: { results: popular },
+        },
+        {
+          data: { results: nowPlaying },
+        },
+        {
+          data: { results: upComing },
+        },
+      ] = await Promise.all([getPopular(), getNowPlay(), getUpComing()]);
       setNowPlaying(nowPlaying);
       setPopular(popular);
       setUpcoming(upComing);
-
     } catch {
-      setError("Not found data! plaese check network")
+      setError("Not found data! plaese check network");
     } finally {
       setLoading(false);
     }
-  }, [])
+  }, []);
 
-  return <>
-    <Helmet>
-      <title>Movies | Kimflix</title>
-    </Helmet>
-    {
-      loading ?
-        (<Loading />) :
+  return (
+    <>
+      <Helmet>
+        <title>Movies | Kimflix</title>
+      </Helmet>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container>
+          {popular && popular.length > 0 && (
+            <Section
+              title="Popular"
+              data={popular}
+              background={true}
+              isMovie={isMovie}
+            />
+          )}
 
-        (<Container>
+          {nowPlaying && nowPlaying.length > 0 && (
+            <Section title="nowPlaying" data={nowPlaying} isMovie={isMovie} />
+          )}
 
-          {
-            popular &&
-            popular.length > 0 &&
-            <Section title="Popular" data={popular} background={true} isMovie={isMovie} />
-          }
-
-          {
-            nowPlaying
-            && nowPlaying.length > 0
-            && <Section title="nowPlaying" data={nowPlaying} isMovie={isMovie} />
-          }
-
-
-
-          {
-            upComing &&
-            upComing.length > 0 &&
+          {upComing && upComing.length > 0 && (
             <Section title="UpComing" data={upComing} isMovie={isMovie} />
-          }
+          )}
           {error && <Message text={error} />}
-        </Container>)
-    }
-  </>
-}
+        </Container>
+      )}
+    </>
+  );
+};
 
 export default Movie;
--- src/components/router.tsx
+++ src/components/router.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "Routes/home";
-import Search from "Routes/search";
-import Movie from "Routes/movie";
-import TV from "Routes/tv";
-import Detail from "Routes/detail";
+import Home from "Screens/home";
+import Search from "Screens/search";
+import Movie from "Screens/movie";
+import TV from "Screens/tv";
+import Detail from "Screens/detail";
 import Header from "./header";
-import List from "Routes/list";
+import List from "Screens/list";
+import styled from "styled-components";
+
+const BodyContainer = styled.div`
+  padding-top: 120px;
+`;
 
 const Router = () => {
   return (
     <BrowserRouter>
       <>
         <Header />
-        <Routes>
-          <Route path="/" element={Home} />
-          <Route path="/movie" element={Movie} />
-          <Route path="/search" element={Search} />
-          <Route path="/tv" element={TV} />
-          <Route path="/movie/:id" element={Detail}></Route>
-          <Route path="/show/:id" element={Detail}></Route>
-          <Route path="/movie/list/:list" element={List}></Route>
-          <Route path="/tv/list/:list" element={List}></Route>
-        </Routes>
+        <BodyContainer>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/tv" element={<TV />} />
+            <Route path="/movie/:id" element={<Detail />}></Route>
+            <Route path="/show/:id" element={<Detail />}></Route>
+            <Route path="/movie/list/:list" element={<List />}></Route>
+            <Route path="/tv/list/:list" element={<List />}></Route>
+          </Routes>{" "}
+        </BodyContainer>
       </>
     </BrowserRouter>
   );
